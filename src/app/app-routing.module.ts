6faa5aae7c3a0b2b5297c72d5ff9d3112772dde3
trevin-livele <liveletrevin6@gmail.com>
2022-10-15 18:14:52 +0300

    ✨ {feat} introduced the routers to specific modules in the app module.ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  
+  {
+    path: 'dashboard',
+    canActivate: [],
+    loadChildren: ()=> import('./dashboard/dashboard.module').then((m)=>m.DashboardModule)
+  },
+
+  {
+    path: 'employees',
+    canActivate: [],
+    loadChildren: () => import('./employees/employees.module').then((m) => m.EmployeesModule),
+  },
+
+ 
+  { path: '**', component: PageNotFoundComponent, canActivate: [] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
